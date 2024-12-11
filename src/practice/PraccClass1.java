a591f58ff32363d031f132639ac62789ae884e7d
anujak <anuja.kannawar24@gmail.com>
2024-12-11 14:27:26 +0530

    new barnch created

--- src/practice/PraccClass1.java
+++ src/practice/PraccClass1.java
@@ -1,5 +1,14 @@
 package practice;
 
 public class PraccClass1 {
+	void printWord() {
+		System.out.print("Hello, its my 1st commit in new branch under new repo");
+	}
+	
+	public static void main(String[] args) {
+		PraccClass1  praccClass1 = new PraccClass1();
+		praccClass1.printWord();
+		}
+	 
+	}
 
-}

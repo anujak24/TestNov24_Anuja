d752a6adebb52e25fa06163d999c782a82021241
anujak <anuja.kannawar24@gmail.com>
2024-12-14 11:05:05 +0530

    test

--- src/practice/PraccClass2.java
+++ src/practice/PraccClass2.java
@@ -1,5 +1,16 @@
 package practice;
 
 public class PraccClass2 {
+	
+	void m3() {
+		System.out.println("Hello, added m3 ");
+		System.out.println("Branch b change");
+		int i =100;
+		System.out.println(i);
+	}
+	
+	void m4() {
+		System.out.println("");
+	}
 
 }
